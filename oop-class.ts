a3f020a32b93f14f90b8c95b919b7a6914c19f76
Pasindu Sampath <104705509+pasindusampath@users.noreply.github.com>
2023-08-01 00:11:29 +0530

    implemented

--- oop-class.ts
+++ oop-class.ts
@@ -1,57 +1,93 @@
 class Student{
     //We can use access modifiers in Type Script so we can change the direct accessibility
     //private , public , protected
-    private _name:string;
+    //If we use older way of declaring constructor we need to add fields like this
+    /*private _name:string;
     private _age:number;
     private _address:string;
-    private _contact:string;
+    private _contact:string;*/
 
     //->    We cannot create more than one constructor here as java
     //->    Only we can do is declare parameters with ? and the meaning of it is the parameters
     //      Not mandatory
     //->    We can also add access modifier to the Constructor
-    public constructor(name?:string,age?:number,address?:string,contact?:string) {
+    //->    Older Way of declaring constructor
+    //->    More code need to be written
+
+   /* public constructor(name?:string,age?:number,address?:string,contact?:string) {
         this._name = name;
         this._age = age;
         this._address = address;
         this._contact = contact;
+    }*/
+    constructor(private name?:string,private age?:number,private address?:string,private contact?:string) {
+
     }
+
     //->    The getters and setters for inject data and access data
-
-    get name(): string {
-        return this._name;
-    }
-
-    set name(value: string) {
+    //->    We need these things if We use Older Way of declaring constructor
+    /*set name(value:string){
         this._name = value;
     }
-
-    get age(): number {
-        return this._age;
+    get name():string{
+        return this._name;
     }
-
-    set age(value: number) {
+    set age(value:number){
         this._age = value;
     }
-
-    get address(): string {
-        return this._address;
+    get age():number{
+        return this._age;
     }
-
-    set address(value: string) {
+    set address(value:string){
         this._address = value;
     }
-
-    get contact(): string {
+    get address():string{
+        return this._address;
+    }
+    set contact(value:string){
+        this._contact = value;
+    }
+    get contact():string{
         return this._contact;
+    }*/
+
+
+
+    //We Have To Create these methods to set and get data if we use modern way to declare constructor
+    getName(): string {
+        return this.name;
     }
 
-    set contact(value: string) {
-        this._contact = value;
+    setName(value: string) {
+        this.name = value;
+    }
+
+    getAge(): number {
+        return this.age;
+    }
+
+    setAge(value: number) {
+        this.age = value;
+    }
+
+    getAddress(): string {
+        return this.address;
+    }
+
+    setAddress(value: string) {
+        this.address = value;
+    }
+
+    getContact(): string {
+        return this.contact;
+    }
+
+    setContact(value: string) {
+        this.contact = value;
     }
 
     public isEqual(student:Student) : boolean{
-       return  student._name === this._name
+       return  student.name === this.name
     }
 
 }
@@ -67,9 +103,9 @@ function test(){
     let student2:Student = new Student('Pasindu',30,'Habarana','+94 78 714 9579')
 
     //Calling Setter and setting age
-    student.age=20;
+    student.setAge(20);
 
-    console.log(student.age)
+    console.log(student.getAge())
     console.log(student2.isEqual(student))
 
 }
